e39d9c4b9537d83263fbb488ff8452e1b01dd0be
Mahi <agrawalmahi18012006@gmail.com>
2026-06-08 12:42:21 +0530

    Add Day-1 files

---
+++ gcr-codebase/programming-elements/Day-1/Kilometrestomiles.java
@@ -0,0 +1,7 @@
+public class Kilometrestomiles {
+    public static void main(String[] args) {
+        double kilometers = 5.0; 
+        double miles = kilometers * 0.621371; 
+        System.out.println(miles);
+    }
+}
