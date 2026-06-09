d3bdde4a90801022d2f57d9830924bca5a80e961
Mahi <agrawalmahi18012006@gmail.com>
2026-06-09 11:18:59 +0530

    Add Level-2 Programming elements

---
+++ core-java-practice/gcr-codebase/programming-elements/Day-1/Level2/TotalPrice.java
@@ -0,0 +1,14 @@
+package Level2;
+import java.util.Scanner;
+
+public class TotalPrice {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            double unitPrice = sc.nextDouble();
+            int quantity = sc.nextInt();
+            double total = unitPrice * quantity;
+            System.out.println("The total purchase price is INR " + total + " if the quantity " + quantity + " and unit price is INR " + unitPrice);
+        }
+    }
+}
+
