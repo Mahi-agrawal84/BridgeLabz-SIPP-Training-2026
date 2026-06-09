382364b8d8672676babf421d36e74e8c2beeed26
Mahi <agrawalmahi18012006@gmail.com>
2026-06-09 11:09:35 +0530

    Add Level-1 Programming elements

---
+++ core-java-practice/gcr-codebase/programming-elements/Day-1/RaviCodingguild.java
@@ -0,0 +1,22 @@
+public class RaviCodingguild {
+    public static void main(String[] args) {
+
+        String name = "Ravi";
+        int age = 22;
+        char rank = 'A';
+        double salary = 50000.0;
+        float membershipFee = 1500.50f;
+
+        double annualBonus = salary * 0.12;
+        int bonusInt = (int) annualBonus;
+
+        System.out.println("Welcome Card");
+        System.out.println("Name           : " + name);
+        System.out.println("Age            : " + age);
+        System.out.println("Rank           : " + rank);
+        System.out.println("Salary         : " + salary);
+        System.out.println("Membership Fee : " + membershipFee);
+        System.out.println("Annual Bonus   : " + bonusInt);
+        System.out.println("");
+    }
+}
